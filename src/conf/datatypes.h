e57dd5dd72caca80d3891826f84aebc814efe476
acheronfail <acheronfail@gmail.com>
2026-05-02 12:12:52 +0930

    acheronfail/status-current-colours

--- src/conf/datatypes.h
+++ src/conf/datatypes.h
@@ -130,7 +130,7 @@ typedef struct {
 
 typedef struct {
     uint16_t idle_timeout;
-    float duty_threshold;
+    float status_threshold;
     float red_bar_percentage;
     bool show_sensors_while_running;
     float brightness_headlights_on;
